797bc5eb81744f1ad8112468f8e9bd9bee00abe7
Jefte Keller <jeftekeller.dev@gmail.com>
2021-07-30 15:49:43 -0300

    feat: created a view to render registration form, create the user, serializers and register the urls.

--- cnab/settings.py
+++ cnab/settings.py
@@ -132,7 +132,13 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = '/api/accounts/login/'
+LOGIN_REDIRECT_URL = '/api/cnab/'
