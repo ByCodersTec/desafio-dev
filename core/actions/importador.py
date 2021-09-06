20d8ad6a0b7d860b371355cabdda26493ba15745
= <=>
2021-09-06 17:51:02 -0300

    adicionando imports

---
+++ core/actions/importador.py
@@ -0,0 +1,22 @@
+from rest_framework.decorators import action
+from rest_framework import viewsets
+
+from core.models import (
+    Cnab
+)
+from core.serializers.serializer import (
+    CnabSerializer
+)
+
+class ImportadorViewSet(viewsets.ModelViewSet):
+    queryset = Cnab
+    serializer_class = CnabSerializer
+
+    @action(detail=False, methods=["get"], url_path="cnab")
+    def importar_cnab(self, request, *args, **kwargs):
+        
+        print("============")
+        print(request.FILES)
+        print("============")
+        #serializer = PonteirosSerializer(queryset)
+        #return Response(serializer.data)
