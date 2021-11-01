d7498c47efe887d93b30fbe6ccf5c4613c61de91
Filipe Tagliacozzi <filipednb@gmail.com>
2021-11-01 13:23:22 -0300

    Implementa migration e estrutura inicial das tabelas

---
+++ src/main/java/com/github/filipednb/financialtransactions/FinancialTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.github.filipednb.financialtransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialTransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/filipednb/financialtransactions/FinancialTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.filipednb.financialtransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
