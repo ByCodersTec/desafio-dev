07e763d2e3f5154e162f07c74c6c93b7c3d1b41c
Filipe Tagliacozzi <filipednb@gmail.com>
2021-11-02 18:08:15 -0300

    Implementa exception handler

---
+++ src/main/java/com/github/filipednb/financialtransactions/api/exception/ApiExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.github.filipednb.financialtransactions.api.exception;
+
+import com.github.filipednb.financialtransactions.file.exception.FileParsingException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(FileParsingException.class)
+    public ResponseEntity<Object> handleUnprocessableFile(FileParsingException ex) {
+        Map<String, Object> body = getExceptionBoddy(ex);
+        return new ResponseEntity<>(body, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(DateTimeException.class)
+    public ResponseEntity<Object> handleDateException(FileParsingException ex) {
+        Map<String, Object> body = getExceptionBoddy(ex);
+        return new ResponseEntity<>(body, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    private Map<String, Object> getExceptionBoddy(FileParsingException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+        return body;
+    }
+}
