abb5e45d03e359fe237253f94059909ca20fc060
Pedro Rodrigues <pedro.rodrigues@hfocus.com.br>
2022-02-11 18:08:58 -0300

    Enable express urlencoded

--- backend/api/src/server.ts
+++ backend/api/src/server.ts
@@ -4,6 +4,7 @@ import router from './routes';
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(process.env.SERVER_PORT || 3000);
