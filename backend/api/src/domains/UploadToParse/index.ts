33831713c9e7fbd77bb281bc1f35a7bdc63a9136
Pedro Rodrigues <pedro.rodrigues@hfocus.com.br>
2022-02-14 21:09:17 -0300

    Integrate parse entity when receive a file from upload form

--- backend/api/src/domains/UploadToParse/index.ts
+++ backend/api/src/domains/UploadToParse/index.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from "express";
-import { uuid } from "uuidv4";
+import { Op } from "sequelize";
+import { v5 as uuidv5, v4 as uuidv4 } from "uuid";
+
+import ParserStatus from "../../entities/ParserStatus";
+import PostgresRepostory from "../../infra/repository/PostgresRepository";
 import KafkaService from "../../infra/services/kafka";
 
+const repository = new PostgresRepostory(ParserStatus);
 
 const handler = async (request: Request, response: Response) => {
   try {
@@ -14,11 +19,30 @@ const handler = async (request: Request, response: Response) => {
     if (mimetype !== "text/plain") {
       throw new Error(`${mimetype} is invalid format. Available formats ["text/plain"]`)
     }
-    
+
+    const value = buffer.toString('base64');
+    const { key, hasher } = {
+      key: uuidv4(),
+      hasher: uuidv5(value, "1b671a64-40d5-491e-99b0-da01ff1f3341")
+    }
+
+    const parserStatus = await repository.findOne({ hasher, status: { [Op.not]: "error" } });
+
+    if (parserStatus) {
+      throw new Error("Transactions block already processed. Try another!");
+    }
+
     const service = new KafkaService();
     await service.send({
-      key: uuid(),
-      value: buffer.toString('base64')
+      key,
+      value
+    });
+
+    repository.save({
+      id: key,
+      hasher,
+      count: 0,
+      message: 'Processing started'
     })
 
     return response.status(200).send();
