9f817e99b92ec4d97c0a2c6727395a3d19e0c7b5
Pedro Rodrigues <pedro.rodrigues@hfocus.com.br>
2022-02-15 14:13:09 -0300

    Created auth component UI and connected with redux provider

--- frontend/src/components/Auth/index.js
+++ frontend/src/components/Auth/index.js
@@ -1,8 +1,72 @@
+import { useEffect, useState } from 'react';
+import { Formik, Form } from 'formik';
+import { connect } from 'react-redux';
 
+import { useHistory } from 'react-router-dom';
+import * as Yup from "yup";
 
-const AuthComponent = () => {
-  return <h1>Authenticate</h1>
+import Preloader from '../Preloader';
+
+import { Container, LoginFormContainer, FormGroup, FormSubmitButton } from './styles';
+import { getLoginDispatch } from '../../store/modules/login/actions';
+
+const Schema = Yup.object().shape({
+    email: Yup.string().email("Provide a valid email").required("This field is required"),
+    password: Yup.string().required("This field is required"),
+});
+
+const LoginPage = ({ loading, error, getLoginDispatch }) => {
+
+    const history = useHistory();
+    const initialValues = {email: "", password: ""};
+
+    const handleSubmitForm = (values) => getLoginDispatch(values, history);
+
+    return <Container>
+        <LoginFormContainer>
+            <Formik initialValues={initialValues} onSubmit={handleSubmitForm} validationSchema={Schema}>
+                {({values, errors, handleBlur, handleChange}) => {
+                    return <Form>
+                        {error && <p className="error">
+                            {error}
+                            </p>}
+                        {loading && <Preloader />}
+                        <FormGroup>
+                            <label>Email</label>
+                            <input
+                                type="text"
+                                name="email"
+                                placeholder="Email"
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                                />
+                            {errors.email && 
+                                <span className="error"> {errors.email} </span>}
+                        </FormGroup>
+                        <FormGroup>
+                            <label>Password</label>
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;"
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                                />
+                            {errors.password && 
+                                <span className="error"> {errors.password} </span>}
+                        </FormGroup>
+                        <FormSubmitButton type="submit">
+                            Connect
+                        </FormSubmitButton>
+                    </Form>
+                }}
+            </Formik>
+        </LoginFormContainer>
+    </Container>
 }
 
-
-export default AuthComponent;
+const mapStateToProps = (state) => {
+    return { loading: state.login.loading, error: state.login.error}
+};
+  
+export default connect(mapStateToProps, { getLoginDispatch })(LoginPage);
