d48b3ee4b4706d14b23dd55a9779bbd8b560eb9b
Pedro Rodrigues <pedro.rodrigues@hfocus.com.br>
2022-02-15 14:11:51 -0300

    Added frontend stack redux store provider and defined global styles as default import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
-import { GlobalStyles } from './styles/GlobalStyles';
+import GlobalStyles from './styles/GlobalStyles';
+
+import store from './store'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
     <GlobalStyles />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('application')
 );
