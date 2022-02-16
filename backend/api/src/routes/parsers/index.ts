ce97c655cd1dd026793d3d20cb13fe461c990d40
Pedro Rodrigues <pedro.rodrigues@hfocus.com.br>
2022-02-16 16:58:42 -0300

    Added delete resource on api routes

--- backend/api/src/routes/parsers/index.ts
+++ backend/api/src/routes/parsers/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { getAllController } from '../../domains/ParserStatus/GetAllParsers';
 import { getController } from '../../domains/ParserStatus/GetParser';
+import { deleteController } from '../../domains/ParserStatus/DeleteParser';
 import { updateController, updateSchema } from '../../domains/ParserStatus/UpdateParser';
 
 const router = Router();
 
 router.get('/', (request, response) => getAllController.handle(request, response));
 router.get('/:id', (request, response) => getController.handle(request, response));
+router.delete('/:id', (request, response) => deleteController.handle(request, response));
 router.put('/:id', updateSchema, (request, response) => updateController.handle(request, response));
 
 export default router;
