ef0830526c618103a770dee7be9ac1c75a3a8720
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2022-03-28 21:14:44 -0300

    Add basic project

---
+++ server/src/main/java/com/alexandrecampos/devchallenge/DevchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.alexandrecampos.devchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevchallengeApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/alexandrecampos/devchallenge/DevchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandrecampos.devchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
