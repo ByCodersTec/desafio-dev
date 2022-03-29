39b35e785e8e07e3c52a3286ed6317c5449be754
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2022-03-29 01:20:12 -0300

    Clean project up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Arquivo de Controle Bancário</h1>
     </div>
   );
-}
-
-export default App;
+};
