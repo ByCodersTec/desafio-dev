2b57edd9ab5a40c9d11e664b04469e88a7e55c87
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2022-03-30 08:33:12 -0300

    Allow swagger in spring security

--- server/src/main/java/com/alexandrecampos/devchallenge/controller/CnabController.java
+++ server/src/main/java/com/alexandrecampos/devchallenge/controller/CnabController.java
@@ -6,6 +6,7 @@ import com.alexandrecampos.devchallenge.model.CnabSummaryDto;
 import com.alexandrecampos.devchallenge.request.CnabListRequest;
 import com.alexandrecampos.devchallenge.response.CnabFileResponse;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,11 +14,14 @@ import org.springframework.web.multipart.MultipartFile;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public interface CnabController {
     @GetMapping
+    @PreAuthorize("isAuthenticated()")
     GenericPage<CnabDto> list(CnabListRequest request);
 
     @GetMapping("summary")
+    @PreAuthorize("isAuthenticated()")
     CnabSummaryDto summary(@RequestParam(required = false) String documentId, @RequestParam(required = false) String cardNumber);
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PreAuthorize("hasRole('developer') or #id == #jwt.subject")
     CnabFileResponse upload(@RequestPart MultipartFile file);
 }
