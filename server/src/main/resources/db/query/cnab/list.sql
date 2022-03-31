e228c68e121cdf3b645b054cfdce594daa70efa5
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2022-03-30 22:00:23 -0300

    Fix filtering

--- server/src/main/resources/db/query/cnab/list.sql
+++ server/src/main/resources/db/query/cnab/list.sql
@@ -16,9 +16,13 @@ from
 	core.cnab c
 	inner join core.operation_type ot on c.operation_type_id = ot.id
 where
-	coalesce(:CARD_NUMBER, '') = ''
-	or card_number like concat('%', :CARD_NUMBER)
-	and coalesce(:DOCUMENT_ID, '') = ''
-	or document_id = :DOCUMENT_ID
+	(
+		coalesce(:CARD_NUMBER, '') = ''
+		or card_number like concat('%', :CARD_NUMBER)
+	)
+	and (
+		coalesce(:DOCUMENT_ID, '') = ''
+		or document_id = :DOCUMENT_ID
+	)
 limit
 	:SIZE offset :PAGE * :SIZE
