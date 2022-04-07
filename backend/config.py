7c1c5f0074336b9dc66a282a98368ffca45621ae
leonardoademir <leoademir0@gmail.com>
2022-04-07 00:24:47 -0300

    executar comando, alterações no banco
    na raiz backend/app, no terminal
    >> py
    >> from app import db
    >> db.drop_all()
    >> db.create_all()

--- backend/config.py
+++ backend/config.py
@@ -1,16 +1,32 @@
 import os
-SECRET_KEY = os.urandom(32)
-# Grabs the folder where the script runs.
-basedir = os.path.abspath(os.path.dirname(__file__))
-# Enable debug mode.
-DEBUG = True
-DB_USER = 'root'
-DB_PASS = 'root'
-DB_HOST = 'localhost'
-DB_PORT = '3306'
-DB_NAME = 'cnab_bycoders'
+from tkinter import FALSE
 
-# Connect to the database
-SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-# Turn off the Flask-SQLAlchemy event system and warning
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class Config(object):
+    TESTING= False
+    DEBUG= False
+
+class Development(Config):
+    SECRET_KEY = os.urandom(32)
+    # Grabs the folder where the script runs.
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    # Enable debug mode.
+    DEBUG = True
+    DB_USER = 'root'
+    DB_PASS = 'root'
+    DB_HOST = 'localhost'
+    DB_PORT = '3306'
+    DB_NAME = 'cnab_bycoders'
+
+    # Connect to the database
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    # Turn off the Flask-SQLAlchemy event system and warning
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class Testing(Config):
+    TESTING = True
+
+config = {
+    'development' : Development,
+    'testing': Testing
+}
