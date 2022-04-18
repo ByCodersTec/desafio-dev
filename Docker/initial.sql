3f36f411d43d1dd237eb691ffc9c8a7ff356698a
Ednezer Rezende <godoirezende@gmail.com>
2022-04-18 18:15:18 -0300

    Bycoders - Arquivo ddl banco

--- Docker/initial.sql
+++ Docker/initial.sql
@@ -1,8 +1,3 @@
-CREATE SCHEMA public AUTHORIZATION postgres;
-
-COMMENT ON SCHEMA public IS 'standard public schema';
-
--- DROP SEQUENCE public.hibernate_sequence;
 
 CREATE SEQUENCE public.hibernate_sequence
 	INCREMENT BY 1
@@ -12,15 +7,6 @@ CREATE SEQUENCE public.hibernate_sequence
 	CACHE 1
 	NO CYCLE;
 
--- Permissions
-
-ALTER SEQUENCE public.hibernate_sequence OWNER TO postgres;
-GRANT ALL ON SEQUENCE public.hibernate_sequence TO postgres;
--- public.cnab definition
-
--- Drop table
-
--- DROP TABLE public.cnab;
 
 CREATE TABLE public.cnab (
 	id int8 NOT NULL,
@@ -34,18 +20,6 @@ CREATE TABLE public.cnab (
 	CONSTRAINT cnab_pkey PRIMARY KEY (id)
 );
 
--- Permissions
-
-ALTER TABLE public.cnab OWNER TO postgres;
-GRANT ALL ON TABLE public.cnab TO postgres;
-
-
--- public.tipotransacao definition
-
--- Drop table
-
--- DROP TABLE public.tipotransacao;
-
 CREATE TABLE public.tipotransacao (
 	tipo int4 NOT NULL,
 	descricao varchar(255) NULL,
@@ -54,18 +28,7 @@ CREATE TABLE public.tipotransacao (
 	CONSTRAINT tipotransacao_pkey PRIMARY KEY (tipo)
 );
 
--- Permissions
 
-ALTER TABLE public.tipotransacao OWNER TO postgres;
-GRANT ALL ON TABLE public.tipotransacao TO postgres;
-
-
-
-
--- Permissions
-
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO public;
 INSERT INTO public.tipotransacao
 (tipo, descricao, natureza, sinal )
 VALUES(1,'Débito', 'Entrada', '+');
