dba79f91dd405be425b240d054e1fc6d1b71cd09
Ednezer Rezende <godoirezende@gmail.com>
2022-04-18 18:05:45 -0300

    Bycoders - Alterando script initial.sql para criacao de sequence

--- Docker/initial.sql
+++ Docker/initial.sql
@@ -1,10 +1,27 @@
-CREATE TABLE public.tipotransacao (
-	tipo int4 NOT NULL,
-	descricao varchar(255) NULL,
-	natureza varchar(255) NULL,
-	sinal varchar(255) NULL,
-	CONSTRAINT tipotransacao_pkey PRIMARY KEY (tipo)
-);
+CREATE SCHEMA public AUTHORIZATION postgres;
+
+COMMENT ON SCHEMA public IS 'standard public schema';
+
+-- DROP SEQUENCE public.hibernate_sequence;
+
+CREATE SEQUENCE public.hibernate_sequence
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+-- Permissions
+
+ALTER SEQUENCE public.hibernate_sequence OWNER TO postgres;
+GRANT ALL ON SEQUENCE public.hibernate_sequence TO postgres;
+-- public.cnab definition
+
+-- Drop table
+
+-- DROP TABLE public.cnab;
+
 CREATE TABLE public.cnab (
 	id int8 NOT NULL,
 	cpf varchar(255) NULL,
@@ -17,6 +34,38 @@ CREATE TABLE public.cnab (
 	CONSTRAINT cnab_pkey PRIMARY KEY (id)
 );
 
+-- Permissions
+
+ALTER TABLE public.cnab OWNER TO postgres;
+GRANT ALL ON TABLE public.cnab TO postgres;
+
+
+-- public.tipotransacao definition
+
+-- Drop table
+
+-- DROP TABLE public.tipotransacao;
+
+CREATE TABLE public.tipotransacao (
+	tipo int4 NOT NULL,
+	descricao varchar(255) NULL,
+	natureza varchar(255) NULL,
+	sinal varchar(255) NULL,
+	CONSTRAINT tipotransacao_pkey PRIMARY KEY (tipo)
+);
+
+-- Permissions
+
+ALTER TABLE public.tipotransacao OWNER TO postgres;
+GRANT ALL ON TABLE public.tipotransacao TO postgres;
+
+
+
+
+-- Permissions
+
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO public;
 INSERT INTO public.tipotransacao
 (tipo, descricao, natureza, sinal )
 VALUES(1,'Débito', 'Entrada', '+');
