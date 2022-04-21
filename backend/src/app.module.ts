e5fc67cf0b487611adfa6b18414cc1c96f0710ab
Enias Oliveira <eniasoliveira27@gmail.com>
2022-04-21 04:49:48 -0300

    chore(wip): adds database/prisma setup for backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ backend/src/database/database.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
+
+@Module({
+  providers: [PrismaService]
+})
+export class DatabaseModule {}
