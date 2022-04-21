8dfb5ecda72209a4c707b0f8ed9ee8d2663f0c42
Enias Oliveira <eniasoliveira27@gmail.com>
2022-04-21 20:44:51 -0300

    feat(wip): addsl transactions file upload endpoint

--- backend/src/http/transactions/transactions.controller.ts
+++ backend/src/http/transactions/transactions.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { TransactionType } from '@prisma/client';
 import { TransactionsService } from 'src/services/transactions/transactions.service';
 
 @Controller('transactions')
 export class TransactionsController {
-    constructor(private readonly transactionsService: TransactionsService) {
-
-    }
+    constructor(private readonly transactionsService: TransactionsService) { }
 
     @Get('types')
     async getTransactionsTypes(): Promise<TransactionType[]> {
         return this.transactionsService.transactionsTypes()
+    }
+
+    @Post('Upload')
+    @UseInterceptors(FileInterceptor('file'))
+    async uploadTransactionsFile(@UploadedFile() file: Express.Multer.File) {
+        console.log(file);
     }
 }
