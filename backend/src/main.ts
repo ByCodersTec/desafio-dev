d64acbfb89ef8d83c2be0e981147f92d77ddc758
Enias Oliveira <eniasoliveira27@gmail.com>
2022-04-23 08:32:06 -0300

    hotfix: adds Port env to backend server setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5500);
+  await app.listen(process.env.PORT || 5500);
 }
 bootstrap();
