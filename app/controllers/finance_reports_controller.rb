a87a1954cde3b0bfa2da93dcc9f1fc436174b0b7
Milles Dyson Schroeder <milles.schroeder@gmail.com>
2022-05-29 23:45:31 -0300

    feat: Finance movement listing and filter

--- app/controllers/finance_reports_controller.rb
+++ app/controllers/finance_reports_controller.rb
@@ -7,6 +7,13 @@ class FinanceReportsController < ApplicationController
   def show
     @finance_report = FinanceReport.find(params[:id])
     @finance_report_movement = @finance_report.finance_movements
+    @stores_for_select = @finance_report_movement.pluck(:store_name).uniq
+    if params[:store_filter]
+      @finance_report_movement = @finance_report_movement.where(store_name: params[:store_filter] ) unless params[:store_filter] == ""
+      @filtered_store = params[:store_filter]
+    else
+      @filtered_store = ""
+    end
   end
 
   def filter
