7ce5b72d1666e57bfa2c24b0b06855373d5418f5
Milles Dyson Schroeder <milles.schroeder@gmail.com>
2022-05-30 23:50:59 -0300

    fix: Store Index and Show styling standardization

--- app/controllers/store_financial_movements_controller.rb
+++ app/controllers/store_financial_movements_controller.rb
@@ -5,14 +5,7 @@ class StoreFinancialMovementsController < ApplicationController
   end
 
   def show
-    @finance_report = StoreFinancialMovement.find(params[:id])
-    @finance_report_movement = @finance_report.finance_movements
-    @stores_for_select = @finance_report_movement.pluck(:store_name).uniq
-    if params[:store_filter]
-      @finance_report_movement = @finance_report_movement.where(store_name: params[:store_filter] ) unless params[:store_filter] == ""
-      @filtered_store = params[:store_filter]
-    else
-      @filtered_store = ""
-    end
+    @store_movement = StoreFinancialMovement.find(params[:id])
+    @store_finance_movements = @store_movement.finance_movements
   end
 end
