b8a88524b2c84e7d44f7da6b17455a23787d0641
Milles Dyson Schroeder <milles.schroeder@gmail.com>
2022-05-31 01:14:31 -0300

    fix: Fix menu active class

--- app/javascript/packs/dashboard.js
+++ app/javascript/packs/dashboard.js
@@ -3,9 +3,8 @@
 function setActive() {
   const menuItems = document.getElementById('menu-list').querySelectorAll("a")
   let url = window.location.href;
-  console.log(menuItems)
   menuItems.forEach(el => {
-    if (window.location.pathname.includes("/finance_reports/new")){
+    if (window.location.pathname.includes("/finance_reports/new") || window.location.pathname === "/"){
       document.querySelector(".link-import").classList.add('active');
     }else {
       document.querySelector(".link-reports").classList.add('active');
