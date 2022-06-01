19ee3d1227b32f7a0e8956f383cb8b14808e7082
Milles Dyson Schroeder <milles.schroeder@gmail.com>
2022-05-31 23:12:11 -0300

    build: Implementing Simplecov Coverage Report

--- Gemfile
+++ Gemfile
@@ -59,6 +59,7 @@ group :test do
   gem 'webdrivers'
   gem 'shoulda-matchers', '~> 5.0'
   gem 'rails-controller-testing'
+  gem 'simplecov'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+SimpleCov.start
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
