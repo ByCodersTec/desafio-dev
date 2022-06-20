164a59e651956025f9fcc42c957ffccf4a0c6ebe
SensiateLeo <leonardo.sensiate@hotmail.com>
2022-06-20 19:29:25 -0300

    feat: implement upload file to server

--- frontend/src/components/FileInput.js
+++ frontend/src/components/FileInput.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useRef, useState } from 'react';
+import databaseService from '../service/database.service'
 import './FileInput.css'
 
 const FileInput = () => {
     const inputRef = useRef(null);
-    const [file, setFile] = useState({});
+    const [file, setFile] = useState(null);
     
     const handleChange = function(e) {
       e.preventDefault();
       if (e.target.files && e.target.files[0]) {
-        setFile(e.target.files[0]);
+        const file = e.target.files[0];
+        setFile(file);
       }
     };
     
@@ -16,14 +18,23 @@ const FileInput = () => {
       inputRef.current.click();
     };
 
+    const saveFile = async () => {
+      const data = new FormData();
+      data.append("file", file);
+    
+      databaseService.saveCNAB(data).then(() => {
+        setFile(null);
+      });
+    };
+
     useEffect(() => {
-      if (JSON.stringify(file) !== '{}')
-        console.log(file);
+      if (file) saveFile();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [file]);
     
     return (
       <form  className='form'  onSubmit={(e) => e.preventDefault()}>
-        <input className='inputFile' ref={inputRef} type='file' multiple={false} onChange={handleChange} />
+        <input className='inputFile' ref={inputRef} type='file' multiple={false} accept='.txt' onChange={handleChange} />
         <label className='label'>
           <div>
             <button className='uploadButton' onClick={onButtonClick}>Importar arquivo</button>
