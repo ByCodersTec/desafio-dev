a3da353440723e1c057b9d8f331a560f19f113f7
SensiateLeo <leonardo.sensiate@hotmail.com>
2022-06-20 19:29:02 -0300

    chore: changes to handle new api calls

--- backend/src/main/java/com/desafio/backend/controller/CNABController.java
+++ backend/src/main/java/com/desafio/backend/controller/CNABController.java
@@ -5,32 +5,28 @@ import java.util.List;
 import com.desafio.backend.model.CNAB;
 import com.desafio.backend.repository.CNABRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/desafio/server")
 public class CNABController {
 
     @Autowired
     private CNABRepository cnabRepository;
 
     // get all CNABs
-    @GetMapping("/cnabs")
-    public List<CNAB> getAllEmployees() {
+    @GetMapping("/cnab")
+    public List<CNAB> listCNAB() {
         return cnabRepository.findAll();
     }
 
     // create CNAB rest api
-    @PostMapping("/cnabs")
-    public CNAB createEmployee(@RequestBody CNAB cnab) {
-        return cnabRepository.save(cnab);
+    @PostMapping("/cnab")
+    public void updateCNAB(@RequestParam("file") MultipartFile file) {
+        System.out.println(file);
     }
 }
 
