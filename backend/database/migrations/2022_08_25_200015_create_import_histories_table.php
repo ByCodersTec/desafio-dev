07f6fa2c1b75aedc07f32722b2669baaf8813f07
Ramon Lima <ramonlima08@gmail.com>
2022-08-25 17:03:12 -0300

    create import history migration

---
+++ backend/database/migrations/2022_08_25_200015_create_import_histories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImportHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('import_histories', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('date');
+            $table->string('file');
+            $table->string('status')->default('importado');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('import_histories');
+    }
+}
