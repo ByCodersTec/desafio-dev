bbeeced72b93e182ea9b6a93034387ff9253f0b1
Ramon Lima <ramonlima08@gmail.com>
2022-08-25 18:45:11 -0300

    add info to Doc

--- backend/app/Traits/ApiResponserTrait.php
+++ backend/app/Traits/ApiResponserTrait.php
@@ -6,13 +6,25 @@ use Illuminate\Http\Response;
 
 trait ApiResponserTrait
 {
-    
+
+    /**
+     * Build a seccess response
+     * @param string|array $data
+     * @param int $code
+     * @return Illuminate\Http\JsonResonse
+     */
     public function successResponse($data=[], $code = Response::HTTP_OK)
     {
         return response()->json(['data' => $data], $code);
     }
 
-    public function errorResponse(string $message, int $code)
+    /**
+     * Build a error response
+     * @param string $message
+     * @param int $code
+     * @return Illuminate\Http\JsonResonse
+     */
+    public function errorResponse($message, $code)
     {
         return response()->json(['error' => $message, 'code' => $code], $code);
     }
