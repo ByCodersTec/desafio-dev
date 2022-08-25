7cd7e55a7014598a908142afa65e9919c23f3ff4
Ramon Lima <ramonlima08@gmail.com>
2022-08-25 16:58:51 -0300

    Create model TypeTransaction

---
+++ backend/app/Models/TypeTransaction.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TypeTransaction extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 
+        'type'
+    ];
+}
