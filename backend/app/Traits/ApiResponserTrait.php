ea581d8a9ee4b73020690608145ba78e149205e1
Ramon Lima <ramonlima08@gmail.com>
2022-08-25 18:33:23 -0300

    methods sucess & error

--- backend/app/Traits/ApiResponserTrait.php
+++ backend/app/Traits/ApiResponserTrait.php
@@ -4,4 +4,14 @@ namespace App\Traits;
 
 trait ApiResponserTrait
 {
+    
+    public function successResponse(array $data=[], int $code) : string
+    {
+        return response()->json(['data' => $data], $code);
+    }
+
+    public function errorResponse(string $message, int $code) : string
+    {
+        return response()->json(['error' => $message, 'code' => $code], $code);
+    }
 }
