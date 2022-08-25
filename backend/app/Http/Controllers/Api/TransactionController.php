41da189dc628dcc6404e8119d538e7e14a2e2b3c
Ramon Lima <ramonlima08@gmail.com>
2022-08-25 17:55:13 -0300

    import model Transaction

--- backend/app/Http/Controllers/Api/TransactionController.php
+++ backend/app/Http/Controllers/Api/TransactionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
