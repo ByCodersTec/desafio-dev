971c814e1f364d3499901914378c52011f26a2f9
Ramon Lima <ramonlima08@gmail.com>
2022-08-25 18:44:08 -0300

    fix bug return

--- backend/app/Traits/ApiResponserTrait.php
+++ backend/app/Traits/ApiResponserTrait.php
@@ -2,15 +2,17 @@
 
 namespace App\Traits;
 
+use Illuminate\Http\Response;
+
 trait ApiResponserTrait
 {
     
-    public function successResponse(array $data=[], int $code) : string
+    public function successResponse($data=[], $code = Response::HTTP_OK)
     {
         return response()->json(['data' => $data], $code);
     }
 
-    public function errorResponse(string $message, int $code) : string
+    public function errorResponse(string $message, int $code)
     {
         return response()->json(['error' => $message, 'code' => $code], $code);
     }
