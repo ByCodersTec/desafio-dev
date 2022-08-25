2f6747f0246091e11cb81edfeb8c286ea97e00d8
Ramon Lima <ramonlima08@gmail.com>
2022-08-25 17:03:26 -0300

    create import history model

---
+++ backend/app/Models/ImportHistory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ImportHistory extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'date', 
+        'file',
+        'status'
+    ];   
+}
