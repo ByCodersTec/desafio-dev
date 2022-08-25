3ad87b16f8a6747bafd108732e7556d881ba6d58
Ramon Lima <ramonlima08@gmail.com>
2022-08-25 18:59:40 -0300

    upload e doc info

--- backend/app/Services/TransactionService.php
+++ backend/app/Services/TransactionService.php
@@ -2,11 +2,28 @@
 
 namespace App\Services;
 
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+
 class TransactionService
 {
-    
-    public function uploadFile($file)
+
+    /**
+     * Process upload file
+     *
+     * @param UploadedFile $file
+     * @return string
+     */
+    public function uploadFile(UploadedFile $file): string
     {
-        
+        try {
+            
+            return Storage::putFile('uploads/imports', $file);
+
+        } catch (\Throwable $th) {
+            Log::error('TransactionService - uploadFile error: ' . $th->getMessage());
+            throw $th;
+        }
     }
 }
