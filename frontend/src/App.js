0f8c40addd5bd17fed900fe4633d6211944ee039
Ramon Lima <ramonlima08@gmail.com>
2022-08-25 21:59:17 -0300

    Add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home"
+import Transaction from "./Pages/Transaction"
+import Import from "./Pages/Import"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/transacao" element={<Transaction />} />
+        <Route path="/importacao" element={<Import />} />
       </Routes>
     </Router>
   );
