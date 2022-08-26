aa1ef58589c0cfec79c8924110ebc93efc29ee36
Ramon Lima <ramonlima08@gmail.com>
2022-08-25 21:59:01 -0300

    Add page import

---
+++ frontend/src/Pages/Import/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Navigation from "../../Components/Navbar";
+import './styles.css'
+
+const Import = () => {
+    return (
+        <div className="container">
+            <Navigation />
+
+            <div className="content">
+
+                <h1>Estou na Importação</h1>
+        
+            </div>
+        </div>
+        
+    )
+}
+
+export default Import;
