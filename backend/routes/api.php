2894a0b4b484a0c0e59405098ad8b9ac72f319fc
Ramon Lima <ramonlima08@gmail.com>
2022-08-25 23:05:53 -0300

    Add route back for summary

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,5 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('summary', [\App\Http\Controllers\Api\TransactionController::class,'summary']);
 Route::get('transaction', [\App\Http\Controllers\Api\TransactionController::class,'index']);
 Route::post('transaction/import', [\App\Http\Controllers\Api\TransactionController::class,'import']);
