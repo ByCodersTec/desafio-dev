18b0268fcc98df58a60d804f20b44e86dd4caad5
Ramon Lima <ramonlima08@gmail.com>
2022-08-25 21:54:40 -0300

    Add page home

---
+++ frontend/src/Pages/Home/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Navigation from "../../Components/Navbar";
+import './styles.css'
+
+const Home = () => {
+    return (
+        <div className="container">
+            <Navigation />
+
+            <div className="content">
+
+                <h1>Estou na home a</h1>
+        
+            </div>
+        </div>
+        
+    )
+}
+
+export default Home;
