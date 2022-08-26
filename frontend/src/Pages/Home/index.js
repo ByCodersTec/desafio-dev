161aca6b82acfbc8a2581a8ab7c66c4a69c99935
Ramon Lima <ramonlima08@gmail.com>
2022-08-25 22:25:35 -0300

    Organize information home

--- frontend/src/Pages/Home/index.js
+++ frontend/src/Pages/Home/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Navigation from "../../Components/Navbar";
+import Card from "../../Components/Card";
 import './styles.css'
 
 const Home = () => {
@@ -8,8 +9,35 @@ const Home = () => {
             <Navigation />
 
             <div className="content">
+                <br />
+                <h1>Dashboard</h1>
 
-                <h1>Estou na home a</h1>
+                <Card>
+                    <h3>Resumo de Transações</h3>
+                    <br />
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td style={{width:'30%'}}>Arquivos importados</td>
+                                <td>55</td>
+                                <td style={{width:'30%'}}>Total de Registros Importados</td>
+                                <td>221</td>
+                            </tr>
+                            <tr>
+                                <td>Total de Lojas</td>
+                                <td>7</td>
+                                <td>Total de Crédito</td>
+                                <td>R$ 1521,33 </td>
+                            </tr>
+                            <tr>
+                                <td>Total de Débito</td>
+                                <td>R$ 1521,33</td>
+                                <td>Saldo</td>
+                                <td>R$ 1521,33</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </Card>
         
             </div>
         </div>
