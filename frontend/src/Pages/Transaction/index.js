87a50819608f1746f1e9c13dd673ea8652f6e7b6
Ramon Lima <ramonlima08@gmail.com>
2022-08-25 21:58:50 -0300

    Add page transaction

---
+++ frontend/src/Pages/Transaction/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Navigation from "../../Components/Navbar";
+import './styles.css'
+
+const Transaction = () => {
+    return (
+        <div className="container">
+            <Navigation />
+
+            <div className="content">
+
+                <h1>Estou na Transações</h1>
+        
+            </div>
+        </div>
+        
+    )
+}
+
+export default Transaction;
