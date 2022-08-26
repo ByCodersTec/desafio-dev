502723c9dd61c5c4bbaa0468e8115165d38127e3
Ramon Lima <ramonlima08@gmail.com>
2022-08-25 22:24:35 -0300

    add component Card

---
+++ frontend/src/Components/Card/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = ( props ) => {
+    return (
+        <div className='card'>
+            {props.children}
+        </div>
+    )
+}
+
+export default Card;
