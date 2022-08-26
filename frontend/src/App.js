9fd1b8ae96b5851f9a8d7ff2699f89bac07b2f4f
Ramon Lima <ramonlima08@gmail.com>
2022-08-26 01:14:32 -0300

    add route new page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home"
 import Transaction from "./Pages/Transaction"
 import Import from "./Pages/Import"
+import Store from "./Pages/Store"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/transacao" element={<Transaction />} />
         <Route path="/importacao" element={<Import />} />
+        <Route path="/transacao/loja" element={<Store />} />
       </Routes>
     </Router>
   );
