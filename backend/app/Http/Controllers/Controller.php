393b8db8f91a4a263e9be4bd92185f30da5381b7
Ramon Lima <ramonlima08@gmail.com>
2022-08-27 12:42:51 -0300

    install and initial configs for  swagger Docum

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -7,6 +7,13 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     title="API para Importação de Arquivo CNAB", 
+ *     version="0.1",
+ *     description="A API tem o objetivo de possiblitar a importação de arquivo CNAB com os dados das movimentações finanaceira de várias lojas e exibir os lançamentos",
+ * ),
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
