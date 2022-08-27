7dab0abfc785aee06783e38594ab9468e0a669a7
Ramon Lima <ramonlima08@gmail.com>
2022-08-27 00:32:40 -0300

    add route to list transactions by store

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('summary', [\App\Http\Controllers\Api\TransactionController::class,'summary']);
 Route::get('transaction', [\App\Http\Controllers\Api\TransactionController::class,'index']);
+Route::get('transaction/store', [\App\Http\Controllers\Api\TransactionController::class,'storeTransactions']);
 Route::post('transaction/import', [\App\Http\Controllers\Api\TransactionController::class,'import']);
