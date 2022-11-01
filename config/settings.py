7b7357821166e096076f0919d038c9baa73c7b4d
MiqSA <eng.miqueias.sa@gmail.com>
2022-11-01 12:02:27 -0300

    Update settings to use docker

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'MY_SECRET_KEY')
-DEVELOP_LOCAL = True
+DEVELOP_LOCAL = False
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
