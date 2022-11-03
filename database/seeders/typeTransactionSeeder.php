66fef209bd29d5a1e24bb8bad6bff3ff6c828350
Cleyton Cabral Soares <55414629+cleiicabral@users.noreply.github.com>
2022-11-02 21:49:20 -0300

    feat-CreateTypeTransactionSeeder

---
+++ database/seeders/typeTransactionSeeder.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Ramsey\Uuid\Uuid;
+
+class typeTransactionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('type_transactions')->insert([
+			'id' => Uuid::uuid6(),
+            'transaction_name' => 'debit',
+            'operation_type' => 'input',
+            'transaction_code' => '1',
+        ]);
+        DB::table('type_transactions')->insert([
+			'id' => Uuid::uuid6(),
+            'transaction_name' => 'bill',
+            'operation_type' => 'output',
+            'transaction_code' => '2',
+        ]);
+        DB::table('type_transactions')->insert([
+			'id' => Uuid::uuid6(),
+            'transaction_name' => 'financing',
+            'operation_type' => 'output',
+            'transaction_code' => '3',
+        ]);
+        DB::table('type_transactions')->insert([
+			'id' => Uuid::uuid6(),
+            'transaction_name' => 'credit',
+            'operation_type' => 'input',
+            'transaction_code' => '4',
+        ]);
+        DB::table('type_transactions')->insert([
+			'id' => Uuid::uuid6(),
+            'transaction_name' => 'loan_receipt',
+            'operation_type' => 'input',
+            'transaction_code' => '5',
+        ]);
+        DB::table('type_transactions')->insert([
+			'id' => Uuid::uuid6(),
+            'transaction_name' => 'sale',
+            'operation_type' => 'input',
+            'transaction_code' => '6',
+        ]);
+        DB::table('type_transactions')->insert([
+			'id' => Uuid::uuid6(),
+            'transaction_name' => 'TED',
+            'operation_type' => 'input',
+            'transaction_code' => '7',
+        ]);
+        DB::table('type_transactions')->insert([
+			'id' => Uuid::uuid6(),
+            'transaction_name' => 'DOC',
+            'operation_type' => 'input',
+            'transaction_code' => '8',
+        ]);
+        DB::table('type_transactions')->insert([
+			'id' => Uuid::uuid6(),
+            'transaction_name' => 'rent',
+            'operation_type' => 'output',
+            'transaction_code' => '9',
+        ]);
+    }
+}
