ee024de841a61255f5be3e0c678456f3e7e26b1a
Cleyton Cabral Soares <55414629+cleiicabral@users.noreply.github.com>
2022-11-02 21:56:16 -0300

    feat-ServiceForValidateCpf

---
+++ app/Http/Services/Utils/ValidateCpfService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Services\Utils;
+
+class ValidateCpfService
+{
+	public static function execute(string $cpf)
+	{
+		// Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
+		if (preg_match('/(\d)\1{10}/', $cpf)) {
+			return false;
+		}
+
+		// Faz o calculo para validar o cpf
+		for ($t = 9; $t < 11; $t++) {
+			for ($d = 0, $c = 0; $c < $t; $c++) {
+				$d += $cpf[$c] * (($t + 1) - $c);
+			}
+			$d = ((10 * $d) % 11) % 10;
+			if ($cpf[$c] != $d) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
