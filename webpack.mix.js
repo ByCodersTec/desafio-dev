e9ac96a5becf75e7a0a8a385a76fb6daa59c0137
Cleyton Cabral Soares <55414629+cleiicabral@users.noreply.github.com>
2022-11-04 03:14:20 -0300

    feat-RenderFilesLaravelMix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+// mix.js('resources/js/app.js', 'public/js')
+// .postCss('resources/css/app.css', 'public/css', [
+// 	//
+// ]);
+
+mix.css('resources/css/uploadFormStyle.css', 'public/css');
+
+mix.js('resources/js/uploadLabelScript.js', 'public/js');
