ebf832fff0f0cbc843acfe3fb79fbb520993d75e
Weverton Matias <weverton-matias@hotmail.com>
2022-11-08 13:50:35 -0300

    Ajusdando o build dos containers.

--- www/config/db.php
+++ www/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '400321',
+    'dsn' => 'mysql:host=db;dbname=yii2basic',
+    'username' => 'teste',
+    'password' => '123456',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
