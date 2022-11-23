7e7e951a8d0dfba7c65ee1c1c160a028ac8d7bc5
gustavogusss <gustavo_metzler@hotmail.com>
2022-11-23 14:27:58 -0300

    Add Spring boot project

---
+++ cnab/src/test/java/com/bycoders/cnab/CnabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bycoders.cnab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
