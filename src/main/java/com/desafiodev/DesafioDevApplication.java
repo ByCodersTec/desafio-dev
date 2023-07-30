6fddd9c9c2f5e0ad477f07f121c22fdd419da618
Leonardo <leonardo_scalabrini@hotmail.com>
2023-07-30 18:07:47 -0300

    Adjusts

--- src/main/java/com/desafiodev/DesafioDevApplication.java
+++ src/main/java/com/desafiodev/DesafioDevApplication.java
@@ -1,12 +1,12 @@
-package com.chat;
+package com.desafiodev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatApplication {
+public class DesafioDevApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ChatApplication.class, args);
+    SpringApplication.run(DesafioDevApplication.class, args);
   }
 }
