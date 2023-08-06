a6eabc357fb3a2fbde6bbdb387a2328cf6056851
Leonardo <leonardo_scalabrini@hotmail.com>
2023-08-06 01:08:29 -0300

    Implemented IaC

--- main.tf
+++ main.tf
@@ -15,47 +15,27 @@ terraform {
 
 provider "google" {
   credentials = var.cloud_credential
-
   project = var.project_id
   region  = var.region
   zone    = var.zone
 }
 
-resource "google_project_service" "run_api" {
-  service = "run.googleapis.com"
-}
-
 resource "google_cloud_run_service" "desafio-dev" {
   name     = "desafio-dev"
-  location = var.region
+    location = var.region
 
-  template {
-    spec {
-      containers {
-        image = "leonardoscalabrini/desafio-dev:9"
+    template {
+      spec {
+        containers {
+          image = "leonardoscalabrini/desafio-dev:9"
+        }
       }
     }
-  }
-
-  autogenerate_revision_name = true
 
   traffic {
     percent         = 100
     latest_revision = true
   }
-
-  depends_on = [google_project_service.run_api]
-
-  lifecycle {
-    ignore_changes = [template.0.spec.0.containers.0.image]
-  }
-}
-
-resource "google_cloud_run_service_iam_member" "run_all_users" {
-  service  = google_cloud_run_service.desafio-dev.name
-  location = google_cloud_run_service.desafio-dev.location
-  role     = "roles/run.invoker"
-  member   = "allUsers"
 }
 
 output "service_url" {
