d4283356202c947b4c2b9602a8c0b3a6ea682b7d
Leonardo <leonardo_scalabrini@hotmail.com>
2023-08-07 23:36:14 -0300

    Implemented IaC

--- main.tf
+++ main.tf
@@ -20,24 +20,33 @@ provider "google" {
   zone    = var.zone
 }
 
-resource "google_cloud_run_v2_job" "desafio-dev" {
-    name     = "desafio-dev"
-    location = var.region
-
-    template {
-      spec {
-        containers {
-          image = "leonardoscalabrini/desafio-dev:latest"
-        }
-      }
-    }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
+resource "google_service_account" "default" {
+  account_id   = "service-account-id"
+  display_name = "Service Account"
 }
 
-output "service_url" {
-  value = google_cloud_run_service.desafio-dev.status[0].url
+resource "google_container_cluster" "primary" {
+  name     = "gke-cluster"
+  location = "us-central1"
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "node-pool"
+  location   = "us-central1"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.default.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 }
