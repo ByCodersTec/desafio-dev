d48d62580206a8ad68758f7f0acd1992687f286b
Flávio de Moraes <flaviodemoraes@gmail.com>
2023-08-19 13:31:24 -0300

    A idéia aqui é a criação das interfaces que serão usadas para criação de transações via EF Core.

---
+++ web-api/src/CNAB.Core/Data/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using CNAB.Core.DomainObjects;
+
+namespace CNAB.Core.Data
+{
+    public interface IRepository<T> : IDisposable where T : IAggregateRoot
+    {
+        IUnitOfWork UnitOfWork { get; }
+    }
+}
---
+++ web-api/src/CNAB.Core/Data/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace CNAB.Core.Data
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> SaveChanges();
+        Task BeginTransactionAsync();
+        Task CommitAsync();
+        void Rollback();
+
+    }
+}
