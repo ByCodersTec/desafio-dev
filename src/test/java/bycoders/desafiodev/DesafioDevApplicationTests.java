543abea6a910d73b79dc340c9bf84738ccaa85b6
Ronaldo Carvalho <ronaldobinho@gmail.com>
2023-08-28 12:08:36 -0300

    "bycoders - Commit inicial"

---
+++ src/test/java/bycoders/desafiodev/DesafioDevApplicationTests.java
@@ -0,0 +1,13 @@
+package bycoders.desafiodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
