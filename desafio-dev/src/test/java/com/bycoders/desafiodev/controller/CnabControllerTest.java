8427ce234e9c861462bd859ff8dff1ec1045e9b0
Ronaldo Carvalho <ronaldobinho@gmail.com>
2023-09-04 08:49:35 -0300

    "bycoders - MYSQL com Docker compose"

--- desafio-dev/src/test/java/com/bycoders/desafiodev/controller/CnabControllerTest.java
+++ desafio-dev/src/test/java/com/bycoders/desafiodev/controller/CnabControllerTest.java
@@ -20,6 +20,7 @@ class CnabControllerTest {
 
 
     public static final String CNAB = "/cnab/";
+    public static final String SUCCESS_MESSAGE = "Cnab file processed successfully.";
 
     @Autowired
     private MockMvc mockMvc;
@@ -29,10 +30,9 @@ class CnabControllerTest {
 
     @Test
     public void shouldValidateSaveCNABSuccesfull() throws Exception {
-        String successMessage = "Cnab file processed successfully.";
         MockMultipartFile mockFile = new MockMultipartFile("file", "mock_cnab.txt", "text/plain", "3201903010000014200096206760174753****3153153453JOÃO MACEDO   BAR DO JOÃO ".getBytes());
 
-        when(service.saveCnab(anyString())).thenReturn(successMessage);
+        when(service.saveCnab(anyString())).thenReturn(SUCCESS_MESSAGE);
 
         mockMvc.perform(MockMvcRequestBuilders.multipart(CNAB + "saveCnab")
                         .file(mockFile))
