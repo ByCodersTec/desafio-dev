9384cc57c7c7528366353d33411ab6a5ec4b497f
Thinally Magno <thinallymagno@gmail.com>
2023-09-19 20:35:04 -0300

    estrutura do projeto

---
+++ src/test/java/br/com/bycoders/cnab/CnabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bycoders.cnab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
